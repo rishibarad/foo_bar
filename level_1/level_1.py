4b377db344d5ee5f6f51ce88683638f0e290ced5
rishib <rishib@umich.edu>
2020-03-18 19:34:46 -0400

    completed level 1

--- level_1/level_1.py
+++ level_1/level_1.py
@@ -2,11 +2,25 @@
 
 
 def solution(x):
-    pass
+    squares = []
+    i = 1
+    while x > 0:
+        if i ** 2 > x:
+            sqr = (i - 1) ** 2
+            x -= sqr
+            i = 1
+            squares.append(sqr)
+        else:
+            i += 1
+
+    return squares
 
 
 def main():
-    solution()
+    test_1 = solution(15324)
+    print("test 1:\n", test_1)
+    test_2 = solution(12)
+    print("test 2:\n", test_2)
     pass
 
 
