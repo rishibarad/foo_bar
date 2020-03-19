320f55e103e5c861f83184d284705279d9ab56b6
rishib <rishib@umich.edu>
2020-03-18 21:09:03 -0400

    added test cases

--- level_2/level_2.py
+++ level_2/level_2.py
@@ -1,12 +1,16 @@
 """level 2"""
 
 
-def solution(x):
-    pass
+def solution(src, dest):
+    num_moves = 0
+    return num_moves
 
 
 def main():
-    solution()
+    test_1 = solution(0, 1)
+    print("test 1:\n", test_1)
+    test_2 = solution(19, 36)
+    print("test 2:\n", test_2)
     pass
 
 
