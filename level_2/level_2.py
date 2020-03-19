14a71882594948ab44d702b0bbf3220b9708f947
rishib <rishib@umich.edu>
2020-03-19 18:12:39 -0400

    updates to level 2

--- level_2/level_2.py
+++ level_2/level_2.py
@@ -1,11 +1,21 @@
 """level 2"""
 
 
+
 def solution(src, dest):
+    # convert to rows and columns
     s_row, d_row = int(src / 8), int(dest / 8)
     s_col, d_col = src % 8, dest % 8
-    print(s_row, s_col)
-    print(d_row, d_col)
+
+    # movement possibilities for the knight
+    knight_moves = [[1, 2], [1, -2], [2, 1], [2, -1], [-1, 2], [-1, -2], [-2, 1], [-2, -1]]
+
+    # initialize board with -1. represents unvisited positions
+    chess_board =
+
+    # queue for storing knight moves with distances. initialized with starting position
+    move_queue = [[s_row, s_col, 0]]
+
 
     num_moves = 0
     return num_moves
