10fea08f79cad094bc4c9b220eb1f819af80b52d
rishib <rishib@umich.edu>
2020-03-19 20:22:06 -0400

    passed my level 2 tests

--- level_2/level_2.py
+++ level_2/level_2.py
@@ -3,22 +3,37 @@
 
 
 def solution(src, dest):
-    # convert to rows and columns
-    s_row, d_row = int(src / 8), int(dest / 8)
-    s_col, d_col = src % 8, dest % 8
+    start_pos = [int(src / 8), src % 8]
+    end_pos = [int(dest / 8), dest % 8]
 
     # movement possibilities for the knight
     knight_moves = [[1, 2], [1, -2], [2, 1], [2, -1], [-1, 2], [-1, -2], [-2, 1], [-2, -1]]
 
-    # initialize board with -1. represents unvisited positions
-    chess_board =
+    # initialize chess board with -1 to represent unvisited positions
+    chess_board = [[-1 for i in range(8)] for j in range(8)]
 
-    # queue for storing knight moves with distances. initialized with starting position
-    move_queue = [[s_row, s_col, 0]]
+    # initialize starting position
+    chess_board[start_pos[0]][start_pos[1]] = 0
+    # queue storing knight moves
+    move_queue = [start_pos]
+
+    while len(move_queue) > 0:
+        curr_pos = move_queue.pop(0)
+        if curr_pos == end_pos:
+            return chess_board[curr_pos[0]][curr_pos[1]]
+        else:
+            for m in knight_moves:
+                new_pos = [curr_pos[0] + m[0], curr_pos[1] + m[1]]
+                # check if position is valid
+                if 0 <= new_pos[0] < 8 and 0 <= new_pos[1] < 8:
+                    # check if position is visited
+                    if chess_board[new_pos[0]][new_pos[1]] == -1:
+                        chess_board[new_pos[0]][new_pos[1]] = chess_board[curr_pos[0]][curr_pos[1]] + 1
+                        move_queue.append(new_pos)
 
 
-    num_moves = 0
-    return num_moves
+
+
 
 
 def main():
