14df3f95b42664e95f7d26f3b65819ee9d9d3d1a
rishib <rishib@umich.edu>
2020-03-19 20:55:37 -0400

    all test cases passed. optimized level 2 solution

--- level_2/level_2.py
+++ level_2/level_2.py
@@ -11,19 +11,21 @@ def solution(src, dest):
     # initialize starting position
     chess_board[start_pos[0]][start_pos[1]] = 0
     # queue storing knight moves
+    if start_pos == end_pos:
+        return 0
     move_queue = [start_pos]
     while len(move_queue) > 0:
         curr_pos = move_queue.pop(0)
-        if curr_pos == end_pos:
-            return chess_board[curr_pos[0]][curr_pos[1]]
-        else:
-            for m in knight_moves:
-                new_pos = [curr_pos[0] + m[0], curr_pos[1] + m[1]]
-                # check if position is valid
-                if 0 <= new_pos[0] < 8 and 0 <= new_pos[1] < 8:
-                    # check if position is visited
-                    if chess_board[new_pos[0]][new_pos[1]] == -1:
-                        chess_board[new_pos[0]][new_pos[1]] = chess_board[curr_pos[0]][curr_pos[1]] + 1
+        for m in knight_moves:
+            new_pos = [curr_pos[0] + m[0], curr_pos[1] + m[1]]
+            # check if position is valid
+            if 0 <= new_pos[0] < 8 and 0 <= new_pos[1] < 8:
+                # check if position is visited
+                if chess_board[new_pos[0]][new_pos[1]] == -1:
+                    chess_board[new_pos[0]][new_pos[1]] = chess_board[curr_pos[0]][curr_pos[1]] + 1
+                    if new_pos == end_pos:
+                        return chess_board[new_pos[0]][new_pos[1]]
+                    else:
                         move_queue.append(new_pos)
 
 
