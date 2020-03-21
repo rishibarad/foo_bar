7dd4c172944958260b3c60f1b4b89e9b4ed52160
rishib <rishib@umich.edu>
2020-03-20 21:16:41 -0400

    Added level 3 test cases

--- level_3/level_3.py
+++ level_3/level_3.py
@@ -6,7 +6,10 @@ def solution(x):
 
 
 def main():
-    solution()
+    test_1 = solution([2, 0, 2, 2, 0])
+    print("test 1:\n", test_1)
+    test_2 = solution([-2, -3, 4, -5])
+    print("test 2:\n", test_2)
     pass
 
 
