7f04ca2e39b9029d598e51a4eaf22614c5e487fb
rishib <rishib@umich.edu>
2020-03-22 16:41:42 -0400

    passed my ten tests

--- level_3/level_3.py
+++ level_3/level_3.py
@@ -5,21 +5,29 @@ def solution(x):
     curr_max, curr_min = 1, 1
     max_prod = None
     pos_check = False
+    neg_check, non_zero = 0, 0
 
     for num in x:
         if num > 0:
             curr_max *= num
             curr_min = min(curr_min * num, 1)
             pos_check = True
+            non_zero += 1
         elif num < 0:
             old_max = curr_max
             curr_max = max(curr_min * num, 1)
             curr_min = old_max * num
+            neg_check = num
+            non_zero += 1
         if max_prod is None or max_prod < curr_max:
             max_prod = curr_max
 
-    if max_prod is None and not pos_check:
-        return 0
+    if non_zero == 0:
+        return str(0)
+    elif non_zero == 1 and neg_check != 0:
+        return str(neg_check)
+    elif max_prod is None and not pos_check:
+        return str(0)
     return str(max_prod)
 
 
@@ -38,6 +46,12 @@ def main():
     print("test 6:\n", test_6)
     test_7 = solution([-8])
     print("test 7:\n", test_7)
+    test_8 = solution([-5, 3])
+    print("test 8:\n", test_8)
+    test_9 = solution([5])
+    print("test 9:\n", test_9)
+    test_10 = solution([0])
+    print("test 10:\n", test_10)
     pass
 
 
