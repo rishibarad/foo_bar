241053b628176079c1fddf972c814dfd726ec4de
rishib <rishib@umich.edu>
2020-03-22 16:27:19 -0400

    fixed some bugs. passing all except test 7

--- level_3/level_3.py
+++ level_3/level_3.py
@@ -2,7 +2,8 @@
 
 
 def solution(x):
-    curr_max, curr_min, max_prod = 1, 1, 1
+    curr_max, curr_min = 1, 1
+    max_prod = None
     pos_check = False
 
     for num in x:
@@ -14,12 +15,12 @@ def solution(x):
             old_max = curr_max
             curr_max = max(curr_min * num, 1)
             curr_min = old_max * num
-        if max_prod < curr_max:
+        if max_prod is None or max_prod < curr_max:
             max_prod = curr_max
 
-    if max_prod == 1 and not pos_check:
+    if max_prod is None and not pos_check:
         return 0
-    return max_prod
+    return str(max_prod)
 
 
 def main():
@@ -28,7 +29,15 @@ def main():
     test_2 = solution([-2, -3, 4, -5])
     print("test 2:\n", test_2)
     test_3 = solution([-7, -1, 4, 5])
-    print("test 2:\n", test_3)
+    print("test 3:\n", test_3)
+    test_4 = solution([0, 5])
+    print("test 4:\n", test_4)
+    test_5 = solution([-5, -1])
+    print("test 5:\n", test_5)
+    test_6 = solution([-1, -1])
+    print("test 6:\n", test_6)
+    test_7 = solution([-8])
+    print("test 7:\n", test_7)
     pass
 
 
